703eecdc8f8b49b5ca4d53cabb6e06c6fc774ea5
Maxim Grynykha <gr.gromax@gmail.com>
2021-04-21 00:21:13 +0300

    fix: static analysis errors

--- src/Terminator/Processes.php
+++ src/Terminator/Processes.php
@@ -185,34 +185,37 @@ final class Processes
 
             $process_string_parts = explode(" $session_name ", $process);
             
+            /** @phpstan-ignore-next-line */
             $process_name_with_id = $process_string_parts[0];
+            
+            /** @phpstan-ignore-next-line */
             $process_session_number_with_consumed_memory = trim($process_string_parts[1]);
 
             $session_number = (int) mb_substr(
                 $process_session_number_with_consumed_memory, 0, 
-                mb_strpos($process_session_number_with_consumed_memory, " ")
+                (int) mb_strpos($process_session_number_with_consumed_memory, " ")
             );
 
             $consumed_memory = trim(mb_substr(
                 $process_session_number_with_consumed_memory, 
-                mb_strlen($session_number)
+                mb_strlen((string) $session_number)
             ));
             
             $consumed_memory = mb_substr(
                 $consumed_memory, 0, 
-                mb_strpos($consumed_memory, " ")
+                (int) mb_strpos($consumed_memory, " ")
             );
 
             $consumed_memory = (int) filter_var($consumed_memory, FILTER_SANITIZE_NUMBER_INT);
 
             $process_id = (int) mb_substr(
                 $process_name_with_id, 
-                mb_strrpos($process_name_with_id, " ") + 1
+                (int) mb_strrpos($process_name_with_id, " ") + 1
             );
 
             $process_name = (string) trim(mb_substr(
                 $process_name_with_id, 0, 
-                mb_strrpos($process_name_with_id, " "))
+                (int) mb_strrpos($process_name_with_id, " "))
             );
 
             $pretty_processes[] = new Process(compact(
